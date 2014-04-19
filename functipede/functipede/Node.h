7aa3df469699e0ee78c922c3623543c714da374a
kynan <kynan.rilee@gmail.com>
2014-04-19 14:44:15 -0700

    Node type compiles but isn't quite right yet.

--- functipede/functipede/Node.h
+++ functipede/functipede/Node.h
@@ -1,9 +1,17 @@
 #pragma once
 
-template <typename In, typename Out> struct Node_impl;
-template <typename ...Ins, typename ...Outs>
-struct Node_impl<types<Ins...>, types<Outs...>> {
-	using value = std::function<std::tuple<std::function<void(Ins)>...>()>;
-};
+template <typename Arg> using Port = std::function<void(Arg)>;
 
-using TestNode = Node_impl<types<int, int>, types<int, int>>;
+template <typename ArgList> struct Ports;
+template <typename ...Args> struct Ports<types<Args...>> : public std::tuple<Port<Args>...> {};
+
+template <typename InPorts, typename OutPorts>
+using Node_guts = std::function<InPorts(OutPorts)>;
+
+template <typename InList, typename OutList> struct Node : public Node_guts<Ports<InList>, Ports<OutList>> {};
+
+// making sure this stuff actually compiles
+using Ports_test = Ports<types<int, int>>;
+using Ports_test2 = Ports<types<int>>;
+using Node_guts_test = Node_guts<Ports_test, Ports_test>;
+using Node_test = Node<types<int, int>, types<int, int>>;

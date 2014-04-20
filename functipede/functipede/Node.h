913cecc055ae8a02586806baa28034054b1cfcae
kynan <kynan.rilee@gmail.com>
2014-04-19 17:33:59 -0700

    compiles with all the right types

--- functipede/functipede/Node.h
+++ functipede/functipede/Node.h
@@ -1,17 +1,27 @@
 #pragma once
 
-template <typename Arg> using Port = std::function<void(Arg)>;
+template <typename Arg> struct Port {
+	typedef std::function<void(Arg)> type;
+};
 
-template <typename ArgList> struct Ports;
-template <typename ...Args> struct Ports<types<Args...>> : public std::tuple<Port<Args>...> {};
+//template <typename ...Args> using Ports = std::tuple<Port<Args>...>;
+template <typename ...Args> struct Ports {
+	typedef std::tuple<typename Port<Args>::type...> type;
+};
 
-template <typename InPorts, typename OutPorts>
-using Node_guts = std::function<InPorts(OutPorts)>;
+template <typename InPorts, typename OutPorts> using Node_guts = std::function<InPorts(OutPorts)>;
 
-template <typename InList, typename OutList> using Node = Node_guts<Ports<InList>, Ports<OutList>>;
+template <typename ...Ins> struct In {
+	typedef typename Ports<Ins...>::type InPorts;
+	template <typename ...Outs> struct Out {
+		typedef typename Ports<Outs...>::type OutPorts;
+		typedef typename Node_guts<InPorts, OutPorts> Node;
+	};
+};
 
 // making sure this stuff actually compiles
 using Ports_test = Ports<types<int, int>>;
 using Ports_test2 = Ports<types<int>>;
 using Node_guts_test = Node_guts<Ports_test, Ports_test>;
-using Node_test = Node<types<int, int>, types<int, int>>;
+using Node_test = In<int, int>::Out<int, int>::Node;
+using Node_test2 = In<int>::Out<int>::Node;
